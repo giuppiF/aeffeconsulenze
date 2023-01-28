fe8d90a56aa691ebbb6f6f9833105c65343de62c
Giuseppe Funicello <g.funicello@gmail.com>
2023-01-28 07:46:19 +0100

    fix nav phone

--- components/hero2.tsx
+++ components/hero2.tsx
@@ -74,6 +74,7 @@ export default function Hero() {
                       <a
                         key={item.name}
                         href={item.href}
+                        onClick={() => setMobileMenuOpen(false)}
                         className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10"
                       >
                         {item.name}
