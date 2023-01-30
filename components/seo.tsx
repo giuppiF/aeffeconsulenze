bc00cfbcb47d2421b022abfa6634be742ce6ec63
Giuseppe Funicello <g.funicello@gmail.com>
2023-01-30 15:20:24 +0100

    seo

---
+++ components/seo.tsx
@@ -0,0 +1,24 @@
+import Head from "next/head";
+
+export const SEO = () => {
+  const siteTitle = "AeFFEe consulenze";
+  const image = "/small_logo_white.png";
+  const description =
+    "Offriamo una soluzione completa per la gestione amministrativa del lavoro domestico.";
+
+  return (
+    <Head>
+      <title>{siteTitle}</title>
+      <meta name="description" content={description} />
+      <meta name="type" property="og:type" content="site" />
+      <meta name="title" property="og:title" content={siteTitle} />
+      <meta name="image" property="og:image" content={image} />
+      <meta property="og:image:alt" content={siteTitle} />
+      <meta property="og:description" content={description} />
+      <meta property="og:site_name" content="AeFFEe consulenze" />
+      <meta property="twitter:card" content="summary" />
+      <meta property="twitter:title" content={siteTitle} />
+      <meta property="twitter:description" content={description} />
+    </Head>
+  );
+};
